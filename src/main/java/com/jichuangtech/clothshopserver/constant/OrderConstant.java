9e784a2075e1076313b7b1f882d52e87526d0a76
zxx <467145219@qq.com>
2017-07-31 23:16:52 +0800

    添加注释

--- src/main/java/com/jichuangtech/clothshopserver/constant/OrderConstant.java
+++ src/main/java/com/jichuangtech/clothshopserver/constant/OrderConstant.java
@@ -7,8 +7,11 @@ public class OrderConstant extends Constant{
     public static final String PAY_STATUS_LIST = "/payStatusList";
     /** 待支付*/
     public static final byte ORDER_UN_PAID = 1;
+    /** 待发货*/
     public static final byte ORDER_UN_SEND = 2;
+    /** 待收货*/
     public static final byte ORDER_WAIT_RECEIVE = 3;
+    /** 已完成*/
     public static final byte ORDER_DONE = 4;
     
 }
