f2f1bf172ea6f1c2f044c3f014e98a8e3086687a
huangbin <575395982@qq.com>
2017-10-26 01:35:36 +0800

    ADD 添加 Token 返回数据

---
+++ src/main/java/com/jichuangtech/clothshopserver/model/Token.java
@@ -0,0 +1,12 @@
+package com.jichuangtech.clothshopserver.model;
+
+/**
+ * Created by Bingo on 2017/10/26.
+ */
+public class Token {
+    public String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+}
